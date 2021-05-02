741e0df44a2fd774cec8cc5dc979e5c754f03f49
Dheeraj <dsoni@ee.iitr.ac.in>
2021-05-02 23:40:52 +0530

    Updates

---
+++ precache-manifest.2aea0323aa9e003e2d9d245a7bf261ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab5f2196c4b7259fb741adbfd7f3177a",
+    "url": "/Airbnb-clone/index.html"
+  },
+  {
+    "revision": "9d7e4c780e9d889a4817",
+    "url": "/Airbnb-clone/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "c9968b69a73a702a3207",
+    "url": "/Airbnb-clone/static/css/main.f2952a0e.chunk.css"
+  },
+  {
+    "revision": "9d7e4c780e9d889a4817",
+    "url": "/Airbnb-clone/static/js/2.967da48a.chunk.js"
+  },
+  {
+    "revision": "6ef57ea73f859e79d7f696b27898a6e5",
+    "url": "/Airbnb-clone/static/js/2.967da48a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9968b69a73a702a3207",
+    "url": "/Airbnb-clone/static/js/main.bf795acd.chunk.js"
+  },
+  {
+    "revision": "2444f55e81115dd66e26",
+    "url": "/Airbnb-clone/static/js/runtime-main.fc129058.js"
+  }
+]);
